1fff7faa7beff191986354899dfeb1b9cb3ded3a
JahadB <beylercahad@gmail.com>
2024-07-11 20:23:53 +0400

    ADDED image carousal(not finished)

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Dropdown Menu
+
 function revealMenu(){
     const menuHidden = document.querySelector(".hidden");
     if(menuHidden.getAttribute("style")==null || 
@@ -14,3 +16,48 @@ function revealMenu(){
 const btn = document.querySelector('#btn');
 
 btn.addEventListener("click", revealMenu);
+
+
+//Image Carousal
+const slides = document.querySelectorAll(".image-item");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+
+let slideIndex = 1;
+
+function nextSlide(){
+    if(slideIndex == 3) {
+        slides[slideIndex - 3].removeAttribute("hidden");
+        slides[slideIndex - 1].setAttribute("hidden", "true");
+        slideIndex = slideIndex - 2;
+    }
+    else if(slides[slideIndex].getAttribute("hidden") != null){
+        slides[slideIndex].removeAttribute("hidden");
+        slideIndex++;
+        slides[slideIndex - 2].setAttribute("hidden", "true");
+    }
+
+    console.log(slideIndex);
+
+}
+
+
+next.addEventListener("click", nextSlide);
+
+
+function prevSlide() {
+    if(slideIndex == 1) {
+        slides[slideIndex + 1].removeAttribute("hidden");
+        slides[slideIndex - 1].setAttribute("hidden", "true");
+        console.log(slideIndex);
+        slideIndex = slideIndex + 2;
+    }
+    else {
+        slides[slideIndex - 2].removeAttribute("hidden");
+        slides[slideIndex - 1].setAttribute("hidden", "true");
+        slideIndex--;
+    }
+
+}
+
+prev.addEventListener("click", prevSlide);
